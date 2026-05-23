afa37ca1ac591a333537fc0a3e150967b8570223
2547077 <2547077@students.wits.ac.za>
2026-05-23 02:57:50 +0200

    checking

--- src/pages/TransactionPayment.js
+++ src/pages/TransactionPayment.js
@@ -11,13 +11,13 @@ import { supabase } from "../supabase";
 import "./TransactionPayment.css";
 import md5 from "blueimp-md5";
 
-// PayFast Sandbox configuration
-const PAYFAST_URL   = "https://sandbox.payfast.co.za/eng/process";
-const MERCHANT_ID   = process.env.REACT_APP_PAYFAST_MERCHANT_ID || "10000100";
-const MERCHANT_KEY  = process.env.REACT_APP_PAYFAST_MERCHANT_KEY || "46f0cd694581a";
-const PASSPHRASE    = process.env.REACT_APP_PAYFAST_PASSPHRASE || "jt7NOE43FZPn";
-const RETURN_URL    = `${window.location.origin}/payment/success`;
-const CANCEL_URL    = `${window.location.origin}/payment/cancel`;
+// PayFast Sandbox configuration - Read from environment variables set at build time
+const PAYFAST_URL   = process.env.REACT_APP_PAYFAST_URL || "https://sandbox.payfast.co.za/eng/process";
+const MERCHANT_ID   = process.env.REACT_APP_PAYFAST_MERCHANT_ID;
+const MERCHANT_KEY  = process.env.REACT_APP_PAYFAST_MERCHANT_KEY;
+const PASSPHRASE    = process.env.REACT_APP_PAYFAST_PASSPHRASE;
+const RETURN_URL    = process.env.REACT_APP_PAYFAST_RETURN_URL || `${window.location.origin}/payment/success`;
+const CANCEL_URL    = process.env.REACT_APP_PAYFAST_CANCEL_URL || `${window.location.origin}/payment/cancel`;
 const NOTIFY_URL    = process.env.REACT_APP_NOTIFY_URL || "";
 
 export default function TransactionPayment() {
